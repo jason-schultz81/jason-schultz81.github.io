b4f2a28db999fbeade8a3931f33655547d990c3a
Jason Schultz <jason.a.schultz81@gmail.com>
2025-04-29 14:03:40 -0500

    Added Descriptive Image Alts

--- JavaScript/portfolio.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('triggerLink').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent the link from navigating to #
-    document.getElementById('dropdownBox').classList.toggle('show');
-  });
